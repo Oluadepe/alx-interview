f7d1d4c0c5a2b8c5c0ff3674e36f285f4a1d4c4b
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-29 21:52:25 +0000

    Pascal triangle code added

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,12 +5,17 @@ Pascal's Triangle function
 
 
 def pascal_triangle(n):
-    triangle = [[1] * (i + 1) for i in range(n)]
-    for i in range(n):
-        for j in range(1, i):
-            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
-    return triangle
+    
+    triangle = []
+    for i in range(1, n+1):
+        row = []
+        for j in range(i):
+            if j == 0 or j == i-1:
+                n = 1
+                row.append(n)
+            else:
+                n = triangle[i-2][j-1] + triangle[i-2][j]
+                row.append(n)
+        triangle.append(row)
 
-n = 5
-for row in pascal_triangle(n):
-    print(row)
+    return triangle
