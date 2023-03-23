3a747fbfdb3c434e2cc97ee85d956ec8567b15e0
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-03-23 23:17:25 +0000

    Applied a fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,17 @@
-#!/usr/bin/python3
-"""Change making module.
-"""
-
-
 def makeChange(coins, total):
-    """Determines the fewest number of coins needed to meet a given
-    amount total when given a pile of coins of different values.
-    """
-    if total <= 0:
-        return 0
-    rem = total
-    coins_count = 0
-    coin_idx = 0
-    sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while rem > 0:
-        if coin_idx >= n:
-            return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
-            coins_count += 1
-        else:
-            coin_idx += 1
-    return coins_count
+    # Initialize the dynamic programming array
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    # Compute the minimum number of coins needed for each total amount
+    for t in range(1, total + 1):
+        for coin in coins:
+            if t - coin >= 0:
+                dp[t] = min(dp[t], dp[t - coin] + 1)
+
+    # Return the result
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
+
