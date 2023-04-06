f521238ae8590e66c1f49999de2cb5649dcaca09
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-04-06 21:45:57 +0000

    applied a fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,19 +1,20 @@
+#!/usr/bin/python3
+    for index in targets:
+        if index % num == 0:
+            targets.remove(index)
+    return targets
+
+
 def isPrime(i):
-    """
-    Check if a number is prime.
-    """
-    if i < 2:
+    if i == 1:
         return False
-    for j in range(2, int(i**0.5) + 1):
+    for j in range(2, i):
         if i % j == 0:
             return False
     return True
 
 
 def findPrimes(n):
-    """
-    Dispatch a given set into prime numbers and non-prime numbers.
-    """
     counter = 0
     target = list(n)
     for i in range(1, len(target) + 1):
@@ -21,22 +22,31 @@ def findPrimes(n):
             counter += 1
             target.remove(i)
             target = _searchMultiple(i, target)
+        else:
+            pass
     return counter
 
 
 def isWinner(x, nums):
     players = {'Maria': 0, 'Ben': 0}
-    for num in nums:
-        cluster = set(range(1, num + 1))
+    cluster = set()
+    for elem in range(x):
+        nums.sort()
+        num = nums[elem]
+        for i in range(1, num + 1):
+            cluster.add(i)
+            if i == num + 1:
+                break
         temp = findPrimes(cluster)
+
         if temp % 2 == 0:
             players['Ben'] += 1
-        else:
+        elif temp % 2 != 0:
             players['Maria'] += 1
+
     if players['Maria'] > players['Ben']:
         return 'Maria'
     elif players['Maria'] < players['Ben']:
         return 'Ben'
     else:
         return None
-
