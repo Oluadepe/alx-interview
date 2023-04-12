d25702e220a8a41876788ef10c406b9081ffe0a3
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-04-12 05:22:46 +0000

    fix function name error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,16 +4,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    remaining_amount = total
+    remain = total
     coins_count = 0
     coin_index = 0
     sorted_coins = sorted(coins, reverse=True)
     num = len(coins)
-    while remaining_amount > 0:
+    while remain > 0:
         if coin_index >= num:
             return -1
-        if remaining_amount - sorted_coins[coin_index] >= 0:
-            remaining_amount -= sorted_coins[coin_index]
+        if remain - sorted_coins[coin_index] >= 0:
+            remain -= sorted_coins[coin_index]
             coins_count += 1
         else:
             coin_index += 1
