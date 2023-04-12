594c674c91d4e468b41949f0ea47ecd9f645ec09
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-04-12 06:08:34 +0000

    Applied a Fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,20 +4,20 @@ def makeChange(coins, total):
     """
     Logic to find the fewest number of coins needed to meet a given amount total.
     """
-    if total <= 0:
+
+if total <= 0:
         return 0
-    remain = total
+    rmt = total
     coins_count = 0
     coin_index = 0
     sorted_coins = sorted(coins, reverse=True)
     num = len(coins)
-    while remain > 0:
+    while rmt > 0:
         if coin_index >= num:
             return -1
-        if remain - sorted_coins[coin_index] >= 0:
-            remain -= sorted_coins[coin_index]
+        if rmt - sorted_coins[coin_index] >= 0:
+            rmt -= sorted_coins[coin_index]
             coins_count += 1
         else:
             coin_index += 1
     return coins_count
-
