49212537b4ba54496aefc2d7fe9fdce8854ee012
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-04-12 04:53:29 +0000

    fix function name error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,19 @@
-def makeChange(coin_values, total_amount):
+def makeChange(coin, total):
     """
     Logic to find the fewest number of coins needed to meet a given amount total.
     """
-    if total_amount <= 0:
+    if total <= 0:
         return 0
-    remaining_amount = total_amount
+    remaining_amount = total
     coin_count = 0
     coin_index = 0
-    sorted_coin_values = sorted(coin_values, reverse=True)
-    num_coins = len(coin_values)
+    sorted_coin = sorted(coin, reverse=True)
+    num_coins = len(coin)
     while remaining_amount > 0:
         if coin_index >= num_coins:
             return -1
-        if remaining_amount - sorted_coin_values[coin_index] >= 0:
-            remaining_amount -= sorted_coin_values[coin_index]
+        if remaining_amount - sorted_coin[coin_index] >= 0:
+            remaining_amount -= sorted_coin[coin_index]
             coin_count += 1
         else:
             coin_index += 1
