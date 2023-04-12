1d47b5a94ca755abe20d0ad1496596a061e4bdd3
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-04-12 06:30:13 +0000

    Refactor Logic

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,7 +3,6 @@
 Making Change
 """
 
-
 def makeChange(coins, total):
     """
     Logic to find the fewest number of coins needed to meet a given amount total.
@@ -11,17 +10,18 @@ def makeChange(coins, total):
 
     if total <= 0:
         return 0
-    rmt = total
+    remaining_total = total
     coins_count = 0
     coin_index = 0
     sorted_coins = sorted(coins, reverse=True)
     num = len(coins)
-    while rmt > 0:
+    while remaining_total > 0:
         if coin_index >= num:
             return -1
-        if rmt - sorted_coins[coin_index] >= 0:
-            rmt -= sorted_coins[coin_index]
+        if remaining_total - sorted_coins[coin_index] >= 0:
+            remaining_total -= sorted_coins[coin_index]
             coins_count += 1
         else:
             coin_index += 1
     return coins_count
+
