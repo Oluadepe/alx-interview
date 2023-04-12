a79d5d3d3f9db2bb9097fedbe26eced580329746
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-04-12 04:23:54 +0000

    Added logic file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,21 @@
-def makeChange(coins, total):
-    # Initialize the dynamic programming array
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    # Compute the minimum number of coins needed for each total amount
-    for t in range(1, total + 1):
-        for coin in coins:
-            if t - coin >= 0:
-                dp[t] = min(dp[t], dp[t - coin] + 1)
-
-    # Return the result
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+def make_change(coin_values, total_amount):
+    """
+    Logic to find the fewest number of coins needed to meet a given amount total.
+    """
+    if total_amount <= 0:
+        return 0
+    remaining_amount = total_amount
+    coin_count = 0
+    coin_index = 0
+    sorted_coin_values = sorted(coin_values, reverse=True)
+    num_coins = len(coin_values)
+    while remaining_amount > 0:
+        if coin_index >= num_coins:
+            return -1
+        if remaining_amount - sorted_coin_values[coin_index] >= 0:
+            remaining_amount -= sorted_coin_values[coin_index]
+            coin_count += 1
+        else:
+            coin_index += 1
+    return coin_count
 
